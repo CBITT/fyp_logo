19ae24ba288b70c6f4b97e6802075e15c6eba08d
cbitt <csbango@yahoo.com>
2018-03-24 18:11:40 +0000

    tidy up

--- amend_annotations.py
+++ amend_annotations.py
@@ -1,30 +1,27 @@
+import os
 from PIL import Image
 import glob
 import csv
 from pathlib import Path
 
 size_list = []
-path = 'path/flickr_logos_27_dataset_images/'
-partial_csv = Path('path/partial.csv')
+path = 'path/to/images/'
+partial_csv = Path('path/to/partial.csv')
+
 
 
 if partial_csv.is_file():
     print('partial.csv already created')
 else:
-
     with open(partial_csv,  'w') as myfile:
         writer = csv.writer(myfile)
         writer.writerow(('im_filename', 'width', 'height'))
 
         for filename in glob.glob(path + '/*.jpg'):
             im=Image.open(filename)
-            filename_cut = filename.strip(path).strip('\\') + 'pg'
-
+            filename_cut = os.path.basename(im.filename)
             sizes = str(im.size).strip('()')
-
             width, height = sizes.split()
-
             writer = csv.writer(myfile)
-
             writer.writerow((filename_cut,width.strip(','),height))
 
