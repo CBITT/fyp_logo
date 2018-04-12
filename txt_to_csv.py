cc87259cfbaa04b4db4a5f5b1a03e2d6f63ec3ec
cbitt <csbango@yahoo.com>
2018-04-12 03:50:43 +0100

    tided up added different method of conversion for test file added references

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -1,29 +1,56 @@
+#Code reference
+#Author(s) name:OliverCollins
+#Date: 04/12/2018
+#Title of program/source code: TXTtoCSV.py
+#Code version: 1
+#Type: computer program
+#Web address or publisher: https://github.com/OliverCollins/TXT-to-CSV/blob/master/TXTtoCSV.py
+#Modified: Yes
+#Modified lines: line 16-37 is added. not relevant to the referenced code
+#                line 53 --> added code to remove last character from the end of lines
+
+
 import csv
 from pathlib import Path
 
-training_set = Path('path/training_set.csv')
+#inintialise location for training and test sets
+training_set = Path('path/to/data/training_set.csv')
+test_set = Path('path/to/data/test_set.csv')
+test_txt = Path('path/to/flickr_logos_27_dataset/flickr_logos_27_dataset_query_set_annotation.txt')
+training_txt = Path('path/to/flickr_logos_27_dataset/flickr_logos_27_dataset_training_set_annotation.txt')
+
+#create new csv from training set
 if training_set.is_file():
     print('training_set.csv already created')
 else:
-    with open('path/flickr_logos_27_dataset_training_set_annotation.txt',
-              'r') as training_as_txt_file:
+    with open(training_txt,'r') as training_as_txt_file:
        separated = (line.strip() for line in training_as_txt_file)
-       lines = (line.split(" ") for line in separated if line)
-       with open('path/training_set.csv', 'w') as training_set:
-            writer = csv.writer(training_set)
-            writer.writerow(('im_filename', 'classname', 'training_subset_of_class','x_min','y_min','x_max','y_max'))
+       lines = (line.split(' ') for line in separated if line)
+       with open(training_set, 'w',newline='') as training:
+            writer = csv.writer(training)
+            writer.writerow(('im_filename', 'classname', 'training_subset_of_class','xmin','ymin','xmax','ymax'))
             writer.writerows(lines)
 
-
-test_set = Path('path/test_set.csv')
+#create new csv from test set
 if test_set.is_file():
     print('test_set.csv already created')
 else:
-    with open('path/flickr_logos_27_dataset_query_set_annotation.txt',
-            'r') as test_as_txt_file:
-        separated = (line.strip() for line in test_as_txt_file)
-        lines = (line.split(" ") for line in separated if line)
-        with open('path/test_set.csv','w') as test_set:
-            writer = csv.writer(test_set)
-            writer.writerow(('im_filename', 'classname'))
-            writer.writerows(lines)
+    with open(test_txt,'r') as txt_test:
+        test = open(test_set, 'w')
+        new_text = txt_test.readlines()
+    features = []
+    line_break = 0
+
+    for i in range(0, len(new_text)):
+        for feature in new_text[i].split():
+            features.append(feature + ',')
+
+    for i in range(0, len(features)):
+        if (line_break == 2):
+            test.write('\n')
+            test.write(features[i])
+            line_break = 1
+        else:
+            test.write(features[i][:-1])
+            line_break += 1
+    test.close()
