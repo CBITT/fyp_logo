50c98648352b30dcd59edd698063347cc9c18dbc
cbitt <csbango@yahoo.com>
2018-04-17 22:04:57 +0100

    added try catch blocks fixed test_set csv heading

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -20,37 +20,44 @@ test_txt = Path('path/to/flickr_logos_27_dataset/flickr_logos_27_dataset_query_s
 training_txt = Path('path/to/flickr_logos_27_dataset/flickr_logos_27_dataset_training_set_annotation.txt')
 
 #create new csv from training set
-if training_set.is_file():
-    print('training_set.csv already created')
-else:
-    with open(training_txt,'r') as training_as_txt_file:
-       separated = (line.strip() for line in training_as_txt_file)
-       lines = (line.split(' ') for line in separated if line)
-       with open(training_set, 'w',newline='') as training:
-            writer = csv.writer(training)
-            writer.writerow(('im_filename', 'classname', 'training_subset_of_class','xmin','ymin','xmax','ymax'))
-            writer.writerows(lines)
+try:
+    if training_set.is_file():
+        print('training_set.csv already created')
+    else:
+        with open(training_txt,'r') as training_as_txt_file:
+           separated = (line.strip() for line in training_as_txt_file)
+           lines = (line.split(' ') for line in separated if line)
+           with open(training_set, 'w') as training:
+                writer = csv.writer(training)
+                writer.writerow(('im_filename', 'classname', 'training_subset_of_class','xmin','ymin','xmax','ymax'))
+                writer.writerows(lines)
+except:
+    print('Error in converting flickr_logos_27_dataset_training_set_annotation.txt to training_set.csv')
 
 #create new csv from test set
-if test_set.is_file():
-    print('test_set.csv already created')
-else:
-    with open(test_txt,'r') as txt_test:
-        test = open(test_set, 'w')
-        new_text = txt_test.readlines()
-    features = []
-    line_break = 0
+try:
+    if test_set.is_file():
+        print('test_set.csv already created')
+    else:
+        with open(test_txt,'r') as txt_test:
+            test = open(test_set, 'w')
+            new_text = txt_test.readlines()
+        features = []
+        line_break = 0
+        test_writer = csv.writer(test)
+        test_writer.writerow(('im_filename', 'classname'))
+        for i in range(0, len(new_text)):
+            for feature in new_text[i].split():
+                features.append(feature + ',')
 
-    for i in range(0, len(new_text)):
-        for feature in new_text[i].split():
-            features.append(feature + ',')
-
-    for i in range(0, len(features)):
-        if (line_break == 2):
-            test.write('\n')
-            test.write(features[i])
-            line_break = 1
-        else:
-            test.write(features[i][:-1])
-            line_break += 1
-    test.close()
+        for i in range(0, len(features)):
+            if (line_break == 2):
+                test.write('\n')
+                test.write(features[i])
+                line_break = 1
+            else:
+                test.write(features[i][:-1])
+                line_break += 1
+        test.close()
+except:
+    print('Error in converting flickr_logos_27_dataset_query_set_annotation.txt to test_set.csv')
